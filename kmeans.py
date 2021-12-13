aec322af1e66f2f6026f23a9ea5daca17a0dd7ad
Achmad Muchlasin <muchlaseen@gmail.com>
2021-12-13 17:28:38 +0700

    Fixing random method & scatter plotting

--- kmeans.py
+++ kmeans.py
@@ -1,114 +1,138 @@
 import math
+import random as rand
 
 class proses:
     def jarak(self,data1,data2):
         n = len(data1)
         total = 0
-
+        
         for i in range (0,n):
-            total = total + math.pow(data1[i]-data2[i],2)
+            total = total+math.pow(data1[i]-data2[i],2)
         return math.sqrt(total)
     
     def getIndexMin(self,data):
-        n=len(data)
-        index=0
-        min=data[0]
-
+        n = len(data)
+        index = 0
+        min = data[0]
+        
         for i in range (1,n):
-            if min > data[i]:
-                index=i
-                min=data[i]
+            if min > data[i]: 
+                index = i
+                min = data[i]
         return index
     
     def mean(self,data):
-        n=len(data)
-        m=len(data[0])
-        tot=[0]*m
-
-        for i in range (n):
+        n = len(data)
+        m = len(data[0])
+        tot = [0]*m
+        for i in range(n):
             for j in range (m):
-                tot[j]=tot[j]+data[i][j]
-        
-        for i in range (m):
-                    tot[i]=tot[i]/n
+                tot[j] = tot[j]+data[i][j]
+        for i in range(m):
+            tot[i] = tot[i]/n
         return tot
-
+    
+    def getMinMax(self, data, fiturke):
+        n = len(data)
+        min = data[0][fiturke]
+        max = data[0][fiturke]
+        for i in range (n):
+            if min > data[i][fiturke]:
+                min = data[i][fiturke]
+            if max < data[i][fiturke]:
+                max = data[i][fiturke]
+        return min, max
+    
 class kmeans(proses):
     data=[]
-    nCluster=[]
+    nCluster=0
     centroid=[]
     jarak=[]
     index=[]
-    error=0
-
-    def __init__(self, data, ncluster):
+    error=[]
+    
+    def __init__(self,data,ncluster):
         kmeans.data = data
         kmeans.nCluster = ncluster
-
+        kmeans.initCentroid(self)
+        
+    def initCentroid(self):
+        cent = []
+        for i in range (self.nCluster):
+            x = []
+            for j in range(len(self.data[0])):
+                [min,max] = proses.getMinMax(self, self.data, j)
+                x.append(rand.randint(min,max))
+            cent.append(x)
+        kmeans.centroid = cent;
+        
     def initCentroidStatic(self):
         arr=[[3,3],[0,1],[8,1]]
-        kmeans.centroid = arr
-
+        kmeans.centroid=arr
+        
     def getJarakSatuDataToCentroids(self,index):
         n=kmeans.nCluster
         jarak=0
         dist=[]
-
-        for i in range (n):
-            jarak = proses.jarak(self,kmeans.data[index],kmeans.centroid[i])
+        
+        for i in range(n):
+            jarak=proses.jarak(self,kmeans.data[index],
+                               kmeans.centroid[i])
             dist.append(jarak)
         return dist
-
+    
     def getAllJarak(self):
         n=len(kmeans.data)
         dist=[]
-
-        for i in range (n):
+        for i in range(n):
             dist.append(self.getJarakSatuDataToCentroids(i))
             kmeans.jarak=dist
-
+            
     def getAnggota(self):
         n=len(kmeans.data)
         x=kmeans.jarak
         ind=[]
-
-        for i in range (n):
+        
+        for i in range(n):
             ind.append(proses.getIndexMin(self,x[i]))
             kmeans.index=ind
-
+            
     def getSumOfDistance(self):
         n=len(self.jarak)
         error=0
-
+        
         for i in range (n):
             error=error+self.jarak[i][self.index[i]]
-            kmeans.error=error
-    
+        kmeans.error=error
+            
     def getCentroid(self):
         n=len(kmeans.jarak)
-        
         for i in range (self.nCluster):
             ang=[]
             for j in range (n):
-                if self.index[j] == i:
+                if self.index[j]==i:
                     ang.append(kmeans.data[j])
-            if ang!=[]:
-                kmeans.centroid[i]=proses.mean(self,ang)
-            else:
-                kmeans.centroid[i]=ang
+                if ang!=[]:
+                    kmeans.centroid[i]=proses.mean(self,ang)
+                else:
+                    kmeans.centroid[i]=ang
+        
+        
+# # #Main Program
+# data=[[1,2],[3,4],[2,3],[9,2],[8,9],[2,6],[8,1],[9,5],[8,9],[5,8],[8,8]]
+# ncluster=3
 
+# #Pembuatan Objek dan Testing 
+# obj_kmeans=kmeans(data,ncluster)
+# obj_kmeans.initCentroidStatic()
+# obj_kmeans.getAllJarak()
+# obj_kmeans.getAnggota()
+# obj_kmeans.getSumOfDistance()
+# obj_kmeans.getCentroid()
 
-# Main Program
-data = [[1, 2], [3, 4], [2, 3], [9, 2], [8, 9], [2, 6], [8, 1], [9, 5], [8, 9], [5, 8], [8, 8]]
-ncluster = 3
+# pro = proses()
+# [min, max] = pro.getMinMax(data, 0)
 
-# Pembuatan objek dan testing
-obj_kmeans = kmeans(data, ncluster)
-obj_kmeans.initCentroidStatic()
-obj_kmeans.getAllJarak()
-obj_kmeans.getAnggota()
-obj_kmeans.getSumOfDistance()
-obj_kmeans.getCentroid()
-
-
-# print("Centroid Baru : ", obj_kmeans.centroid)
+# #Cetak Hasil Utama
+# print("Nilai Minimal : ", min)
+# print("Nilai Maksimal : ", max)
--- learningkmeans.py
+++ learningkmeans.py
@@ -1,68 +1,78 @@
+# %%
 from kmeans import kmeans
 import matplotlib.pyplot as plt
 import numpy as np
 
 class ClusteringKmeans(kmeans):
     error=[]
-
-    def __init__(self, data, ncluster):
+    
+    def __init__(self,data,ncluster):
         kmeans.data=data
         kmeans.nCluster=ncluster
         kmeans.initCentroidStatic(self)
-
+        
     def learning(self):
-        oldError = -1
-        newError = 0
-
-        while oldError != newError:
-            oldError = newError
+        errorLama=-1
+        errorBaru=0
+        
+        while errorLama!=errorBaru:
+            errorLama=errorBaru
             kmeans.getAllJarak(self)
             kmeans.getAnggota(self)
-            kmeans.getSumOfDistance(self)
-            kmeans.initCentroidStatic(self)
-            newError=kmeans.error
-            self.error.append(newError)
-
+            kmeans.getSumOfDistance(self) 
+            kmeans.getCentroid(self)
+            errorBaru=kmeans.error
+            ClusteringKmeans.error.append(errorBaru)
+        
     def plottingError(self):
         plt.plot(self.error)
-        plt.show
-
+        plt.title("Sum of Distance")
+        plt.xlabel("Iterasi")
+        plt.ylabel("Error")
+        plt.show()
+    
     def scatterDataCentroid(self):
-        x=self.kumpulAnggotaPercluster()
+        fig, ax = plt.subplots()
+        x = self.kumpulAnggotaPercluster()
+        tes = []
         for i in range (len(x)):
+            tes.append(i)
             temp=np.array(x[i])
-            plt.scatter(temp[:,0],temp[:,1])
-            cent=np.array(kmeans.centroid)
-            plt.scatter(cent[:,0], cent[:,1], marker='x')
-            plt.show()
+            ax.scatter(temp[:,0], temp[:,1])
+        cent=np.array(kmeans.centroid)
+        plt.legend(tes)
+        ax.scatter(cent[:,0], cent[:,1], marker='x')
+        plt.title("Scatter")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.show()
     
     def kumpulAnggotaPercluster(self):
         n=len(kmeans.data)
         m=kmeans.nCluster
         Datacluster=[]
-
         for i in range (m):
             temp=[]
             for j in range (n):
-                if(i == kmeans.index[j]):
+                if (i==kmeans.index[j]):
                     temp.append(kmeans.data[j])
-                    Datacluster.append(temp)
-            
-            return Datacluster
+            Datacluster.append(temp)
+        return Datacluster
         
-# Main Program
-data = [[1, 2], [3, 4], [2, 3], [9, 2], [8, 9], [2, 6], [8, 1], [9, 5], [8, 9], [5, 8], [8, 8]]
-ncluster = 3
+#Main Program
+data=[[1,2],[3,4],[2,3],[9,2],[8,9],[2,6],[8,1],[9,5],[8,9],[5,8],[8,8]]
+ncluster=3
 
-# Pembuatan objek dan testing
+#Pembuatan Objek dan pemanggilan method learning 
 objek=ClusteringKmeans(data,ncluster)
 objek.learning()
 objek.plottingError()
 objek.scatterDataCentroid()
 
-# # Printing
-# print("Centroid : ", objek.centroid)
-# print("\nIndex anggota : ", objek.index)
-# print("\nSum of Distance semua iterasi: ", objek.error)
-# print("\nSum of Distance akhir: ", objek.error[len(objek.error)-1])
+#Cetak Hasil Utama
+print("centroid =",objek.centroid)
+print("\n index anggota data = ", objek.index)
+print("\n Sum of Distance Semua Iterasi = ",objek.error)
+print("\n Sum Of Distance Akhir =", objek.error[len(objek.error)-1])
+
 # %%
